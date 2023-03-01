aef230c58b5c3451c5544e40648eea35bfe05e37
Anthony Mejia <mejia.anthony96@gmail.com>
2023-03-01 02:08:22 -0800

    added contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 import AboutMe from "./components/AboutMe";
+import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 import { scrollTo } from "./scroll";
 
 function App() {
@@ -10,10 +12,12 @@ function App() {
   }, []);
 
   return (
-    <div className="">
+    <div className="w-screen overflow-hidden">
       <Home />
       <AboutMe />
+      <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }
