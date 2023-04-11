2eaac2a9feae54b514b420814738ea697afc8f2a
AnthonyMejia0 <mejia.anthony96@gmail.com>
2023-04-11 00:37:26 -0700

    added text-shadow

--- src/components/WebProject.jsx
+++ src/components/WebProject.jsx
@@ -10,14 +10,14 @@ function WebProject({ color, name, tools, position, img, desc, site, repo }) {
         style={{ backgroundColor: color, textAlign: position }}
         className={`h-56 lg:h-64 z-0 text-white font-playfair tracking-wide py-8 px-10 2xl:pt-16 2xl:pl-20 space-y-1 xl:space-y-3`}
       >
-        <h2 className="font-bold text-3xl lg:text-5xl">{name}</h2>
-        <p className="font-bold">{tools}</p>
+        <h2 className="font-bold text-3xl lg:text-5xl text-shadow">{name}</h2>
+        <p className="font-bold text-shadow">{tools}</p>
       </div>
 
       <div className="mx-8 -mt-24">
-        <div className="overflow-hidden rounded-sm xs:w-full sm:w-[80%] lg:w-[40%] 2xl:w-[45%] mx-auto mb-8">
+        <div className="overflow-hidden rounded-sm xs:w-full sm:w-[80%] lg:w-[40%] 2xl:w-[45%] mx-auto mb-8 shadow">
           <img
-            className="scale-[100.5%]"
+            className="scale-[100.7%]"
             src={img}
             alt={`${name} Screenshot`}
           />
